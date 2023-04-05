915a4e627042dee36febea5191453a5f57bdf1a8
Oskar Trapczynski <oskardenis167@gmail.com>
2023-04-05 13:28:13 +0200

    add basic files structure

---
+++ src/components/Game.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/game.scss';
+
+const Game = () => {
+  return <div>Game</div>;
+};
+
+export default Game;
---
+++ src/components/Menu.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/menu.scss';
+
+const Menu = () => {
+  return <div>Menu</div>;
+};
+
+export default Menu;
